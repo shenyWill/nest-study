e88461d814ac335864bda3f7f88c6675f468725a
shenyuan.will <shenyuan.will@bytedance.com>
2023-08-03 14:51:33 +0800

    feat: 5种HTTP传输方式实现

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AModule } from './a/a.module';
 import { CController } from './c/c.controller';
 import { RModule } from './r/r.module';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [AModule, RModule],
+  imports: [AModule, RModule, PersonModule],
   controllers: [AppController, CController],
   providers: [AppService],
 })
